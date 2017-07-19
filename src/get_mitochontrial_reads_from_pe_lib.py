7acc22084601fa2955dac8fc51a5ff2bf756d049
Tom Harrop <twharrop@gmail.com>
2017-07-19 17:25:06 +1200

    run extraction script - will be slow as

--- src/get_mitochontrial_reads_from_pe_lib.py
+++ src/get_mitochontrial_reads_from_pe_lib.py
@@ -2,26 +2,28 @@
 
 from Bio import SeqIO
 import gzip
+import tompytools
 
 # IO files
 mt_read_file = 'test/stats/mapped_reads.fastq.gz'
-#pe_read_file = 'data/pe_merged.fastq.gz'
-pe_read_file = 'test.fastq.gz'
+pe_read_file = 'data/pe_merged.fastq.gz'
 output_fq = 'test/stats/mapped_reads_with_qual.fastq'
 
 # index mitochondrial reads
-print("Getting IDs of mapped reads from %s" % mt_read_file)
+tompytools.generate_message("Getting IDs of mapped reads from %s"
+                            % mt_read_file)
 with gzip.open(mt_read_file, "rt") as unzipped_file:
     mt_ids = [x.id for x in SeqIO.parse(unzipped_file, 'fastq')]
 
 # read other reads if they're in the mt_ids
-print("Extracting those reads from %s" % pe_read_file)
+tompytools.generate_message("Extracting those reads from %s" % pe_read_file)
 with gzip.open(pe_read_file, "rt") as unzipped_file:
     mt_reads = [x for x in SeqIO.parse(unzipped_file, 'fastq')
                 if x.id in mt_ids]
 
 # write mt_reads to fastq
-print("Writing reads with original quality scores to %s" % output_fq)
+tompytools.generate_message("Writing reads with original quality scores to %s"
+                            % output_fq)
 SeqIO.write(sequences=mt_reads,
             handle=output_fq,
             format='fastq')
