52db5da38679303b0ad580df99d5811cd342087f
Tom Harrop <twharrop@gmail.com>
2017-07-28 17:41:54 +1200

    better variable name

--- src/rotate_contig.py
+++ src/rotate_contig.py
@@ -7,9 +7,9 @@ fasta_file = 'output/mt_contig/unicycler/assembly.fasta'
 fasta = next(SeqIO.parse(fasta_file, 'fasta'))
 
 # rotate
-nad1_start_b1 = 9048
-first_fragment = fasta[nad1_start_b1 - 1:]
-second_fragment = fasta[:nad1_start_b1 - 1]
+new_start_position = 9048
+first_fragment = fasta[new_start_position - 1:]
+second_fragment = fasta[:new_start_position - 1]
 rotated_fasta = first_fragment + second_fragment
 
 # write output
